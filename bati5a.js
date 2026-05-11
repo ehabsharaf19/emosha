8c808d6798d2fb6e9bf363dcd93e682e365bac18
ehabsharaf19 <ehabsharaf28@gmail.com>
2026-05-11 13:32:31 +0300

    Update bati5a.js

--- bati5a.js
+++ bati5a.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBORppTYoq6wbsoM8cED7T_cW5UHVX5XbE",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:136317351287:web:789100a3fb1f8d84c21d72",
   measurementId: "G-XX6P1BY2EJ"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
