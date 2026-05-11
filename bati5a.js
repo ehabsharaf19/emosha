5c34cf416918e3ee41fa931e1b49df31da1e3739
ehabsharaf19 <ehabsharaf28@gmail.com>
2026-05-11 13:48:59 +0300

    Refactor firebase config to export and add database URL
    Updated firebase configuration to include database URL and export it.

--- bati5a.js
+++ bati5a.js
@@ -1,10 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBORppTYoq6wbsoM8cED7T_cW5UHVX5XbE",
-  authDomain: "emosha-b67a3.firebaseapp.com",
-  projectId: "emosha-b67a3",
-  storageBucket: "emosha-b67a3.firebasestorage.app",
-  messagingSenderId: "136317351287",
-  appId: "1:136317351287:web:789100a3fb1f8d84c21d72",
-  measurementId: "G-XX6P1BY2EJ"
+export const firebaseConfig = {
+  databaseURL: "https://emosha-b67a3-default-rtdb.firebaseio.com/",
 };
+
+// هذا السطر يخبر اللعبة ببدء الاتصال بالمخزن
+console.log("تم تفعيل رابط مخزن إيموشا بنجاح");
